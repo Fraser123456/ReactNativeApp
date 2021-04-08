b92ac58ebf703ff73c464f5af1f9ec3dcb66065e
FraserCarpenter <fraserac3@gmail.com>
2021-04-08 15:53:23 +0200

    Completed Login Form and Screen

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,38 +1,6 @@
 import React, { useState } from "react";
-import { Switch, TextInput } from "react-native";
-
-import AppTextInput from "./app/components/AppTextInput";
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  {
-    lable: "Furniture",
-    value: 1,
-  },
-  {
-    lable: "Clothing",
-    value: 2,
-  },
-  {
-    lable: "Cameras",
-    value: 3,
-  },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState();
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="email" />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
---
+++ DoneWithIt/app/components/forms/AppFormField.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppTextInput from "../AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+
+function AppFormField({ name, ...otherProps }) {
+  const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
+
+  return (
+    <>
+      <AppTextInput
+        {...otherProps}
+        keyboardType="email-address"
+        onBlur={() => setFieldTouched(name)}
+        onChangeText={handleChange(name)}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormField;
